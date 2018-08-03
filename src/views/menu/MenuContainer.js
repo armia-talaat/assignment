922f8b21cfca47339e4dd7c8ca9f050c0a13a8dc
Armia Talaat <armia.talaat@vezeeta.com>
2018-08-03 21:29:02 +0200

    Check type of response

--- src/views/menu/MenuContainer.js
+++ src/views/menu/MenuContainer.js
@@ -3,10 +3,13 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import Spinner from '../../components/spinner/Spinner';
+import { USER } from '../../store/actions/user';
 import '../../shared/Utils.scss';
 
 const mapStateToProps = state => ({
-  userLoaded: state.user.loaded
+  userLoaded: state.user.loaded,
+  responseType: state.user.type,
+  user: state.user.user
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch);
@@ -20,7 +23,7 @@ class MenuContainer extends Component {
   }
   componentDidMount() {}
   render() {
-    if (this.state.isLoading || this.props.userLoaded) {
+    if (this.props.userLoaded && this.props.responseType === USER.GET_USER_DONE) {
       return (
         <div className="center center--horizontal center--vertical full-view">
           <Spinner color="#a9f3d3" />
