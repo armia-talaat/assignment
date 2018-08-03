76c68dbfa062a568886a9e138c167e9b4dec132a
Armia Talaat <armia.talaat@vezeeta.com>
2018-08-03 21:23:54 +0200

    Add `App` Component with `Store` and `Router`

---
+++ src/views/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../store';
+import HeaderLayout from '../navigation/HeaderLayout';
+import MenuContainer from './menu/MenuContainer';
+import './App.scss';
+const tabs = [
+  {
+    title: 'Menu Data',
+    url: '/'
+  }
+];
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Provider store={store}>
+          <HeaderLayout tabs={tabs} homeUrl="/">
+            <Router>
+              <Switch>
+                <Route exact path="/" component={MenuContainer} />
+
+                <Redirect to={'/'} />
+              </Switch>
+            </Router>
+          </HeaderLayout>
+        </Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
