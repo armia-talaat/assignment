3e8140da4b9759f2e1f3ea5f710394f00d95f08f
Armia Talaat <armia.talaat@vezeeta.com>
2018-08-03 21:33:52 +0200

    Add responseType and handle form

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,59 +1,104 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Form, Input, Button } from 'semantic-ui-react';
+import { Form, Input, Button, Message } from 'semantic-ui-react';
 import '../../shared/Utils.scss';
+import { USER } from '../../store/actions/user';
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isLoading: !props.userLoaded,
+      responseType: props.responseType,
       username: '',
       password: ''
     };
   }
-  componentDidMount() {}
-  render() {
-    if (this.state.isLoading || this.props.userLoaded) {
-      return (
-        <div className="center center--horizontal center--vertical full-view">
-          <Form>
-            <Form.Field required>
-              <label>User Name</label>
-              <Input
-                placeholder="User Name"
-                onChange={val => {
-                  this.setState({
-                    username: val
-                  });
-                }}
-              />
-            </Form.Field>
-            <Form.Field required>
-              <label>Password</label>
-              <Input
-                placeholder="Password"
-                type="password"
-                onChange={val => {
-                  this.setState({
-                    password: val
-                  });
-                }}
-              />
-            </Form.Field>
-            <Button
-              type="submit"
-              onClick={() => {
-                this.props.signIn(this.state.username, this.state.password);
-              }}
-            >
-              Submit
-            </Button>
-          </Form>
-        </div>
-      );
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (prevState.responseType !== nextProps.responseType) {
+      return {
+        responseType: nextProps.responseType
+      };
     }
-    return <div />;
+    return null;
+  }
+  getResponseType = responseType => {
+    switch (responseType) {
+      case USER.GET_USER_DONE: {
+        return 'success';
+      }
+      case USER.GET_USER_NOT_FOUND: {
+        return 'error';
+      }
+      case USER.GET_USER_FAIL: {
+        return 'error';
+      }
+      default:
+        return '';
+    }
+  };
+  getResponseMessage = responseType => {
+    switch (responseType) {
+      case USER.GET_USER_DONE: {
+        return 'User logged on Successfully';
+      }
+      case USER.GET_USER_NOT_FOUND: {
+        return 'User not found';
+      }
+      case USER.GET_USER_FAIL: {
+        return 'Something went wrong';
+      }
+      default:
+        return '';
+    }
+  };
+  render() {
+    return (
+      <div className="center center--horizontal center--vertical full-view">
+        <Form
+          success={this.getResponseType(this.state.responseType) === 'success'}
+          error={this.getResponseType(this.state.responseType) === 'error'}
+        >
+          <Form.Field required>
+            <label>User Name</label>
+            <Input
+              placeholder="User Name"
+              onChange={e => {
+                this.setState({
+                  username: e.target.value
+                });
+              }}
+            />
+          </Form.Field>
+          <Form.Field required>
+            <label>Password</label>
+            <Input
+              placeholder="Password"
+              type="password"
+              onChange={e => {
+                this.setState({
+                  password: e.target.value
+                });
+              }}
+            />
+          </Form.Field>
+          <Message
+            success
+            header="Login Success"
+            content={this.getResponseMessage(this.state.responseType)}
+          />
+          <Message error header="Login Failed" content={this.getResponseMessage(this.state.responseType)} />
+          <Button
+            type="submit"
+            onClick={() => {
+              this.props.signIn(this.state.username, this.state.password);
+            }}
+          >
+            Submit
+          </Button>
+        </Form>
+      </div>
+    );
   }
 }
 Login.propTypes = {
