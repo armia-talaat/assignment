1ee9e22f7ca3a0f93e13fb5a9c6fa8fb52eb99a2
Armia Talaat <armia.talaat@vezeeta.com>
2018-08-03 21:28:51 +0200

    Remove unecessary init styling

--- src/views/App.js
+++ src/views/App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../store';
+
 import HeaderLayout from '../navigation/HeaderLayout';
 import MenuContainer from './menu/MenuContainer';
-import './App.scss';
+
 const tabs = [
   {
     title: 'Menu Data',
