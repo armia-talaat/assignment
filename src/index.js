b34d9a3beb843fb13585815eedac4a2bb2bf598a
Armia Talaat <armia.talaat@vezeeta.com>
2018-08-03 18:50:15 +0200

    Init Repo with create-react-app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import 'semantic-ui-css/semantic.min.css';
+import App from './views/App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
