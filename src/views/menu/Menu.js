5f7577e6f58c6db7a95ac0a3eb96ff9e7f116da6
Armia Talaat <armia.talaat@vezeeta.com>
2018-08-04 16:57:15 +0200

    Add functionality for `DeleteItem` and init `EditItem`

--- src/views/menu/Menu.js
+++ src/views/menu/Menu.js
@@ -1,60 +1,78 @@
 import React, { Component, Fragment } from 'react';
+import { List, Accordion } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import Spinner from '../../components/spinner/Spinner';
 import DeleteCategory from '../../modals/DeleteCategory';
 import EditCategory from '../../modals/EditCategory';
-import { List, Accordion } from 'semantic-ui-react';
 import AddCategory from '../../modals/AddCategory';
+import DeleteItem from '../../modals/DeleteItem';
+import EditItem from '../../modals/EditItem';
 
 class Menu extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoading: !props.userLoaded
+      isLoading: !props.userLoaded,
     };
   }
   componentDidMount() {}
 
   setItemStructure = items => {
     if (items.length !== 0) {
-      return items.map(item => {
-        return {
-          key: item.id,
-          title: item.name,
-          content: {
-            content: (
+      return items.map(item => ({
+        key: item.id,
+        title: item.name,
+        content: {
+          content: (
+            <Fragment>
               <List>
                 <List.Item icon="food" content={item.name} />
                 <List.Item icon="info circle" content={item.description} />
                 <List.Item icon="dollar sign" content={item.price} />
               </List>
-            )
-          }
-        };
-      });
+              {this.props.user.type === 'admin' && (
+                <div className="center--horizontal">
+                  {
+                    <EditItem
+                      item={item}
+                      editItem={this.props.editItem}
+                      responseType={this.props.editItemState}
+                      revertState={this.props.editItemStateRevert}
+                    />
+                  }
+                  <DeleteItem item={item} deleteItem={this.props.deleteItem} />
+                </div>
+              )}
+            </Fragment>
+          ),
+        },
+      }));
     }
   };
   setCategoryStructure = categories => {
     if (categories.length !== 0) {
-      return categories.map(category => {
-        return {
-          key: category.id,
-          title: category.name,
-          content: {
-            content: (
-              <Fragment>
-                {this.props.user.type === 'admin' && (
-                  <div className="center--horizontal">
-                    <EditCategory category={category} editCategory={this.props.editCategory} />
-                    <DeleteCategory category={category} deleteCategory={this.props.deleteCategory} />
-                  </div>
-                )}
-                <Accordion.Accordion panels={this.setItemStructure(category.items)} />
-              </Fragment>
-            )
-          }
-        };
-      });
+      return categories.map(category => ({
+        key: category.id,
+        title: category.name,
+        content: {
+          content: (
+            <Fragment>
+              {this.props.user.type === 'admin' && (
+                <div className="center--horizontal">
+                  <EditCategory
+                    category={category}
+                    editCategory={this.props.editCategory}
+                    responseType={this.props.editCategoryState}
+                    revertState={this.props.editCategoryStateRevert}
+                  />
+                  <DeleteCategory category={category} deleteCategory={this.props.deleteCategory} />
+                </div>
+              )}
+              <Accordion.Accordion panels={this.setItemStructure(category.items)} />
+            </Fragment>
+          ),
+        },
+      }));
     }
   };
   render() {
@@ -84,7 +102,7 @@ class Menu extends Component {
   }
 }
 Menu.propTypes = {
-  userLoaded: PropTypes.bool
+  userLoaded: PropTypes.bool,
 };
 
 export default Menu;
--- src/views/menu/MenuContainer.js
+++ src/views/menu/MenuContainer.js
@@ -9,7 +9,11 @@ import {
   deleteStoredCategory,
   editStoredCategory,
   addNewCategory,
-  addCategoryStateRevert
+  addCategoryStateRevert,
+  editCategoryStateRevert,
+  deleteStoredItem,
+  editStoredItem,
+  editItemStateRevert,
 } from '../../store/actions/menu';
 import Menu from './Menu';
 
@@ -19,20 +23,31 @@ const mapStateToProps = state => ({
   user: state.user.user,
   menuItems: state.menu.menu,
   menuState: state.menu.type,
-  addCategoryState: state.menu.addCategoryState
+  addCategoryState: state.menu.addCategoryState,
+  editCategoryState: state.menu.editCategoryState,
 });
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(
-    { getMenu, deleteStoredCategory, editStoredCategory, addNewCategory, addCategoryStateRevert },
-    dispatch
+    {
+      getMenu,
+      deleteStoredCategory,
+      editStoredCategory,
+      addNewCategory,
+      addCategoryStateRevert,
+      editCategoryStateRevert,
+      deleteStoredItem,
+      editStoredItem,
+      editItemStateRevert,
+    },
+    dispatch,
   );
 
 class MenuContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoading: !props.userLoaded
+      isLoading: !props.userLoaded,
     };
   }
   componentDidMount() {
@@ -43,14 +58,24 @@ class MenuContainer extends Component {
       this.props.deleteStoredCategory(categoryId);
     }
   };
-  editCategory = (oldCategoryId, categoryName, categoryID) => {
+  editCategory = (oldCategoryId, categoryName, categoryId) => {
     if (this.props.user.name !== '' && this.props.user.type === 'admin') {
-      this.props.editStoredCategory(oldCategoryId, categoryName, categoryID);
+      this.props.editStoredCategory(oldCategoryId, categoryName, categoryId);
     }
   };
-  addCategory = (categoryName, categoryID) => {
+  addCategory = (categoryName, categoryId) => {
     if (this.props.user.name !== '' && this.props.user.type === 'admin') {
-      this.props.addNewCategory(categoryName, categoryID);
+      this.props.addNewCategory(categoryName, categoryId);
+    }
+  };
+  deleteItem = itemId => {
+    if (this.props.user.name !== '' && this.props.user.type === 'admin') {
+      this.props.deleteStoredItem(itemId);
+    }
+  };
+  editItem = (oldItemId, itemName, itemId, itemDescription, itemPrice) => {
+    if (this.props.user.name !== '' && this.props.user.type === 'admin') {
+      this.props.editStoredItem(oldItemId, itemName, itemId, itemDescription, itemPrice);
     }
   };
   render() {
@@ -66,19 +91,25 @@ class MenuContainer extends Component {
           userStates={USER}
           deleteCategory={this.deleteCategory}
           editCategory={this.editCategory}
+          editCategoryState={this.props.editCategoryState}
+          editCategoryStateRevert={this.props.editCategoryStateRevert}
           addCategory={this.addCategory}
           addCategoryState={this.props.addCategoryState}
           addCategoryStateRevert={this.props.addCategoryStateRevert}
+          deleteItem={this.deleteItem}
+          editItem={this.editItem}
+          editItemState={this.props.editItemState}
+          editItemStateRevert={this.props.editItemStateRevert}
         />
       </div>
     );
   }
 }
 MenuContainer.propTypes = {
-  userLoaded: PropTypes.bool
+  userLoaded: PropTypes.bool,
 };
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps,
 )(MenuContainer);
