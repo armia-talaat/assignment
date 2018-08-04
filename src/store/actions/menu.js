0df28f6377ce4bb26c473e3bc93f77354f7034a3
Armia Talaat <armia.talaat@vezeeta.com>
2018-08-04 16:56:17 +0200

    Init `Items` Actions in `redux`

--- src/store/actions/menu.js
+++ src/store/actions/menu.js
@@ -4,50 +4,106 @@ export const MENU = {
   GET_MENU_FAIL: 'MENU_GET_FAIL',
   CATEGORY: {
     DELETE: 'MENU_DELETE_CATEGORY',
-    EDIT: 'MENU_EDIT_CATEGORY',
+    EDIT: {
+      INIT: 'MENU_EDIT_CATEGORY',
+      SUCCESS: 'MENU_EDIT_CATEGORY_SUCCESS',
+      FAIL: {
+        ID: 'MENU_EDIT_CATEGORY_FAIL_ID',
+        NAME: 'MENU_EDIT_CATEGORY_FAIL_NAME',
+        BOTH: 'MENU_EDIT_CATEGORY_FAIL_BOTH',
+      },
+      REVERT: 'MENU_EDIT_CATEGORY_REVERT',
+    },
     ADD: {
       INIT: 'MENU_ADD_CATEGORY',
       SUCCESS: 'MENU_ADD_CATEGORY_SUCCESS',
       FAIL: {
         ID: 'MENU_ADD_CATEGORY_FAIL_ID',
         NAME: 'MENU_ADD_CATEGORY_FAIL_NAME',
-        BOTH: 'MENU_ADD_CATEGORY_FAIL_BOTH'
+        BOTH: 'MENU_ADD_CATEGORY_FAIL_BOTH',
       },
-      REVERT: 'MENU_ADD_CATEGORY_REVERT'
-    }
-  }
+      REVERT: 'MENU_ADD_CATEGORY_REVERT',
+    },
+  },
+  ITEM: {
+    DELETE: 'MENU_DELETE_ITEM',
+    EDIT: {
+      INIT: 'MENU_EDIT_ITEM',
+      SUCCESS: 'MENU_EDIT_ITEM_SUCCESS',
+      FAIL: {
+        ID: 'MENU_EDIT_ITEM_FAIL_ID',
+        NAME: 'MENU_EDIT_ITEM_FAIL_NAME',
+        BOTH: 'MENU_EDIT_ITEM_FAIL_BOTH',
+      },
+      REVERT: 'MENU_EDIT_ITEM_REVERT',
+    },
+    ADD: {
+      INIT: 'MENU_ADD_ITEM',
+      SUCCESS: 'MENU_ADD_ITEM_SUCCESS',
+      FAIL: {
+        ID: 'MENU_ADD_ITEM_FAIL_ID',
+        NAME: 'MENU_ADD_ITEM_FAIL_NAME',
+        BOTH: 'MENU_ADD_ITEM_FAIL_BOTH',
+      },
+      REVERT: 'MENU_ADD_ITEM_REVERT',
+    },
+  },
 };
 
 export const getMenu = () => ({
-  type: MENU.GET_MENU
+  type: MENU.GET_MENU,
 });
 
 export const getMenuDone = menu => ({
   type: MENU.GET_MENU_DONE,
-  menu
+  menu,
 });
 
 export const getMenuFail = err => ({
   type: MENU.GET_MENU_FAIL,
-  err
+  err,
 });
 
 export const deleteStoredCategory = categoryId => ({
   type: MENU.CATEGORY.DELETE,
-  categoryId
+  categoryId,
 });
 
 export const editStoredCategory = (oldCategoryId, categoryName, categoryId) => ({
-  type: MENU.CATEGORY.EDIT,
+  type: MENU.CATEGORY.EDIT.INIT,
   oldCategoryId,
   categoryName,
-  categoryId
+  categoryId,
 });
+
 export const addNewCategory = (categoryName, categoryId) => ({
   type: MENU.CATEGORY.ADD.INIT,
   categoryName,
-  categoryId
+  categoryId,
 });
+
 export const addCategoryStateRevert = () => ({
-  type: MENU.CATEGORY.ADD.REVERT
+  type: MENU.CATEGORY.ADD.REVERT,
+});
+
+export const editCategoryStateRevert = () => ({
+  type: MENU.CATEGORY.EDIT.REVERT,
+});
+
+export const deleteStoredItem = itemId => ({
+  type: MENU.ITEM.DELETE,
+  itemId,
+});
+
+export const editStoredItem = (oldItemId, itemName, itemId, itemDescription, itemPrice) => ({
+  type: MENU.CATEGORY.EDIT.INIT,
+  oldItemId,
+  itemName,
+  itemId,
+  itemDescription,
+  itemPrice,
+});
+
+export const editItemStateRevert = () => ({
+  type: MENU.ITEM.EDIT.REVERT,
 });
