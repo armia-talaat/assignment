711be7d9f4ea779184e04847b32f277e92b71f56
Armia Talaat <armia.talaat@vezeeta.com>
2018-08-04 16:55:44 +0200

    Init `DeleteItem` Modal

---
+++ src/modals/DeleteItem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Modal, Icon } from 'semantic-ui-react';
+
+const DeleteItem = props => (
+  <Modal trigger={<Button negative>Delete</Button>} closeIcon>
+    <Modal.Header>Delete Item</Modal.Header>
+    <Modal.Content>
+      <p>Are you sure you want to delete this Item?</p>
+      <p>{props.item.name}</p>
+    </Modal.Content>
+    <Modal.Actions>
+      <Button
+        negative
+        onClick={() => {
+          props.deleteItem(props.item.id);
+        }}
+      >
+        <Icon name="delete" /> Delete Item
+      </Button>
+    </Modal.Actions>
+  </Modal>
+);
+DeleteItem.propTypes = {
+  item: PropTypes.object,
+  deleteItem: PropTypes.func.isRequired,
+};
+
+export default DeleteItem;
