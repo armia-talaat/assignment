1bbfc472383d9826dd8818257790bba1903e5727
Armia Talaat <armia.talaat@vezeeta.com>
2018-08-04 05:14:36 +0200

    Add `Utils` include in `index.js`

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import 'semantic-ui-css/semantic.min.css';
+import './shared/Utils.scss';
 import App from './views/App';
 import registerServiceWorker from './registerServiceWorker';
 
