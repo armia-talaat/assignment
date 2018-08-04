643bdd6b98c370417b9b0357703d34fb743f0d82
Armia Talaat <armia.talaat@vezeeta.com>
2018-08-04 16:56:35 +0200

    Add trailing commas for lint

--- src/store/actions/user.js
+++ src/store/actions/user.js
@@ -3,31 +3,31 @@ export const USER = {
   GET_USER_DONE: 'USER_GET_DONE',
   GET_USER_FAIL: 'USER_GET_FAIL',
   GET_USER_NOT_FOUND: 'USER_GET_USER_NOT_FOUND',
-  GET_USER_LOGOUT: 'USER_GET_USER_LOGOUT'
+  GET_USER_LOGOUT: 'USER_GET_USER_LOGOUT',
 };
 
 export const getUser = (userName, userPassword) => ({
   type: USER.GET_USER,
   userName,
-  userPassword
+  userPassword,
 });
 
 export const getUserDone = user => ({
   type: USER.GET_USER_DONE,
-  user
+  user,
 });
 
 export const getUserFail = err => ({
   type: USER.GET_USER_FAIL,
-  err
+  err,
 });
 
 export const getUserNotFound = user => ({
   type: USER.GET_USER_NOT_FOUND,
-  user
+  user,
 });
 
 export const logoutUser = () => ({
   type: USER.GET_USER_LOGOUT,
-  user: { name: '' }
+  user: { name: '' },
 });
